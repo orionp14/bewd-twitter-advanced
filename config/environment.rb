2d1ba184d08d269fed89889b6f185b9f0832f5f0
Orion Piotrowski <opiotrow@asu.edu>
2023-05-18 13:02:11 -0700

    second commit

--- config/environment.rb
+++ config/environment.rb
@@ -1,17 +1,15 @@
-# Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# Initialize the Rails application.
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :address        => ENV['smtp.mailgun.org'],
-  :port           => ENV['587'],
-  :authentication => :plain,
-  :user_name      => ENV['postmaster@sandbox1873f87019be477f960410107374c4c6.mailgun.org'],
-  :password       => ENV['9c6bb8c13de8ca570bf364a45ba0b5e8-db4df449-b5c5e5eb'],
-  :domain         => ENV['sandbox1873f87019be477f960410107374c4c6.mailgun.org'],
-  :enable_starttls_auto => true
+  :address        => ENV['SMTP_ADDRESS'],
+  :port           => ENV['SMTP_PORT'],
+  :authentication => ENV['SMTP_AUTHENTICATION'],
+  :user_name      => ENV['SMTP_USERNAME'],
+  :password       => ENV['SMTP_PASSWORD'],
+  :domain         => ENV['SMTP_DOMAIN'],
+  :enable_starttls_auto => ENV['SMTP_ENABLE_STARTTLS_AUTO']
 }
 
-ActionMailer::Base.default_url_options = { host: ENV['sandbox1873f87019be477f960410107374c4c6.mailgun.org'] }
+ActionMailer::Base.default_url_options = { host: ENV['SMTP_DEFAULT_URL_OPTIONS_HOST'] }
